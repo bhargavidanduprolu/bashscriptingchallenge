8c82bc15fe22be4d91334291fe51fa9fd1847fb9
narendra <tirumaninarendrababu@gmail.com>
2023-07-31 17:45:49 +0530

    bhargavi solution for day1 challange

---
+++ Challenges/day_1_sol_bhargavi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Task 1: to use comments explaining menaingful info
+#Author :Bhargavi
+#Challange TWS Day1
+#Task 2: to print something using echo
+#set -x
+echo "This is my TWS shell scripting challange Day1 solution"
+#Done
+#Task 3: To declare 2 variables and assign values to them
+num1=20
+num2=30
+#Task 4: to use variables defined and print sum of 2 numbers
+echo "sum of num1 and num2=$( expr $num1 + $num2 )"
+#Task 5:using Built-in variables in bash
+echo $USER
+echo ":home directory of user is $HOME :and current working directory is $PWD"
+#Task 6: using wild cards
+WorkDIR=$PWD
+ls "$WorkDIR" | grep '\.sh$'
+
+
